87da67b5d0316aa74f0838ce3100cc335784a65e
minthug <jkisimmortal4@gmail.com>
2024-10-30 15:00:07 +0900

    Add Redis configuration and ItemRedisDto class

---
+++ absurdityAppForJava/src/main/java/forOlderJava/absurdityAppForJava/domain/item/service/response/ItemRedisDto.java
@@ -0,0 +1,21 @@
+package forOlderJava.absurdityAppForJava.domain.item.service.response;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import forOlderJava.absurdityAppForJava.domain.item.Item;
+
+import java.time.LocalDateTime;
+
+public record ItemRedisDto(Long itemId, String name, int price, int discount,
+                           @JsonSerialize(using = LocalDateTimeSerializer.class)
+                           @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+                           LocalDateTime createdAt) {
+
+    public static ItemRedisDto from(final Item item) {
+        return new ItemRedisDto(
+                item.getId(), item.getName(), item.getPrice(), item.getDiscount(),
+                item.getCreatedAt());
+    }
+}
