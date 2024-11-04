7c8c715a6f91d6515dac0fc7e150a95ac6e61820
minthug <jkisimmortal4@gmail.com>
2024-11-04 17:50:24 +0900

    Add CRUD operations for CartItem in CartService

--- absurdityAppForJava/src/main/java/forOlderJava/absurdityAppForJava/domain/cart/service/CartService.java
+++ absurdityAppForJava/src/main/java/forOlderJava/absurdityAppForJava/domain/cart/service/CartService.java
@@ -2,9 +2,14 @@ package forOlderJava.absurdityAppForJava.domain.cart.service;
 
 import forOlderJava.absurdityAppForJava.domain.cart.Cart;
 import forOlderJava.absurdityAppForJava.domain.cart.CartItem;
+import forOlderJava.absurdityAppForJava.domain.cart.exception.NotFoundCartException;
+import forOlderJava.absurdityAppForJava.domain.cart.exception.NotFoundCartItemException;
 import forOlderJava.absurdityAppForJava.domain.cart.repository.CartItemRepository;
 import forOlderJava.absurdityAppForJava.domain.cart.repository.CartRepository;
 import forOlderJava.absurdityAppForJava.domain.cart.service.request.RegisterCartItemCommand;
+import forOlderJava.absurdityAppForJava.domain.cart.service.request.UpdateCartItemCommand;
+import forOlderJava.absurdityAppForJava.domain.cart.service.response.FindCartItemResponse;
+import forOlderJava.absurdityAppForJava.domain.cart.service.response.FindCartItemsResponse;
 import forOlderJava.absurdityAppForJava.domain.item.Item;
 import forOlderJava.absurdityAppForJava.domain.item.exception.NotFoundItemException;
 import forOlderJava.absurdityAppForJava.domain.item.repository.ItemRepository;
@@ -14,6 +19,8 @@ import forOlderJava.absurdityAppForJava.domain.member.repository.MemberRepositor
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -48,4 +55,53 @@ public class CartService {
         return savedCartItem.getId();  // getId() == getCartItemId();
     }
 
+    @Transactional
+    public void deleteCartItem(final Long cartItemId) {
+        CartItem foundCartItem = findCartItemByCartItemId(cartItemId);
+        cartItemRepository.deleteById(cartItemId);
+    }
+
+    @Transactional
+    public FindCartItemsResponse findCartItemsByMemberId(final Long memberId) {
+        Member foundMember = memberRepository.findById(memberId)
+                .orElseThrow(() -> new NotFoundMemberException("해당 사용자를 찾을 수 없습니다"));
+
+        Cart foundCart = cartRepository.findByMember(foundMember)
+                .orElseThrow(() -> new NotFoundCartException("해당 장바구니를 찾을 수 없습니다"));
+
+        List<CartItem> foundCartItems = cartItemRepository.findAllByCartOrderByCreatedAt(foundCart);
+
+        return FindCartItemsResponse.from(foundCartItems
+                .stream()
+                .map(cartItem -> FindCartItemResponse.of(
+                        cartItem.getCart().getId(),
+                        cartItem.getItem().getId(),
+                        cartItem.getQuantity()
+                )).toList());
+    }
+
+    @Transactional
+    public int getCartItemTotalPrice(final long cartItemId) {
+        int totalPrice = 0;
+
+        List<CartItem> cartItems = cartItemRepository.findAllByCartItemIdOrderByCreatedAt(cartItemId);
+
+        for (CartItem cartItem : cartItems) {
+            totalPrice += cartItem.getItem().getPrice() * cartItem.getQuantity();
+        }
+
+        return totalPrice;
+    }
+
+    @Transactional
+    public void updateCartItemQuantity(UpdateCartItemCommand command) {
+        CartItem foundCartItem = findCartItemByCartItemId(command.cartId());
+
+        foundCartItem.changeQuantity(command.quantity());
+    }
+
+    private CartItem findCartItemByCartItemId(Long cartItemId) {
+        return cartItemRepository.findById(cartItemId)
+                .orElseThrow(() -> new NotFoundCartItemException("장바구니 상품이 존재하지 않습니다"));
+    }
 }
