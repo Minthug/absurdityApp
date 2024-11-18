c5a6847984b7307649ee7d8eb320455acaae0f6b
minthug <jkisimmortal4@gmail.com>
2024-11-18 16:49:27 +0900

    Add caching and retrieval of member info in MemberCacheService

--- absurdityAppForJava/src/main/java/forOlderJava/absurdityAppForJava/domain/member/service/MemberCacheService.java
+++ absurdityAppForJava/src/main/java/forOlderJava/absurdityAppForJava/domain/member/service/MemberCacheService.java
@@ -6,16 +6,34 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberCacheService {
 
     private final RedisTemplate<String, Object> redisTemplate;
+    private static final String MEMBER_INFO_PREFIX = "member:info";
     private static final String ACTIVE_MEMBERS_KEY = "active_members";
     private static final String POPULAR_YOUNGER_KEY = "popular_younger";
+
+    public void cacheMemberInfo(MemberRedisDto memberRedisDto) {
+        redisTemplate.opsForValue().set(
+            MEMBER_INFO_PREFIX + memberRedisDto.memberId(),
+                memberRedisDto,
+                Duration.ofHours(24)
+        );
+    }
+
+    // 멤버 정보 조회
+    public Optional<MemberRedisDto> getMemberInfo(Long memberId) {
+        return Optional.ofNullable(
+                (MemberRedisDto) redisTemplate.opsForValue().get(MEMBER_INFO_PREFIX + memberId)
+        );
+    }
 
     public void saveActiveMember(MemberRedisDto memberRedisDto) {
         redisTemplate.opsForZSet().add(
@@ -31,5 +49,9 @@ public class MemberCacheService {
                 youngerRedisDto,
                 youngerRedisDto.rating()
         );
+    }
+
+    public void deleteMemberCache(Long memberId) {
+        redisTemplate.delete(MEMBER_INFO_PREFIX + memberId);
     }
 }
