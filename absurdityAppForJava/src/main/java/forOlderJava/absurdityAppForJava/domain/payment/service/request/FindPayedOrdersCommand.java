921eb06e99dc9a88420adc7762f2743da42de9eb
minthug <jkisimmortal4@gmail.com>
2024-11-24 16:46:07 +0900

    Add builder pattern to FindPayedOrdersCommand and test case

--- absurdityAppForJava/src/main/java/forOlderJava/absurdityAppForJava/domain/payment/service/request/FindPayedOrdersCommand.java
+++ absurdityAppForJava/src/main/java/forOlderJava/absurdityAppForJava/domain/payment/service/request/FindPayedOrdersCommand.java
@@ -1,5 +1,8 @@
 package forOlderJava.absurdityAppForJava.domain.payment.service.request;
 
+import lombok.Builder;
+
+@Builder
 public record FindPayedOrdersCommand(Long memberId, int page) {
     public static FindPayedOrdersCommand of(final Long memberId, final int page) {
         return new FindPayedOrdersCommand(memberId, page);
--- absurdityAppForJava/src/test/java/forOlderJava/absurdityAppForJava/OrderFlowIntegrationTest.java
+++ absurdityAppForJava/src/test/java/forOlderJava/absurdityAppForJava/OrderFlowIntegrationTest.java
@@ -2,6 +2,8 @@ package forOlderJava.absurdityAppForJava;
 
 import forOlderJava.absurdityAppForJava.domain.member.service.MemberService;
 import forOlderJava.absurdityAppForJava.domain.member.service.request.RegisterUserCommand;
+import forOlderJava.absurdityAppForJava.domain.order.entity.OrderStatus;
+import forOlderJava.absurdityAppForJava.domain.order.exception.UnauthorizedOrderException;
 import forOlderJava.absurdityAppForJava.domain.order.service.OrderService;
 import forOlderJava.absurdityAppForJava.domain.order.service.request.CreateOrderRequest;
 import forOlderJava.absurdityAppForJava.domain.order.service.request.CreateOrderRequest.CreateOrderItemRequest;
@@ -9,6 +11,8 @@ import forOlderJava.absurdityAppForJava.domain.order.service.request.CreateOrder
 import forOlderJava.absurdityAppForJava.domain.order.service.response.FindOrderDetailResponse;
 import forOlderJava.absurdityAppForJava.domain.order.service.response.FindOrderResponse;
 import forOlderJava.absurdityAppForJava.domain.order.service.response.FindOrdersResponse;
+import forOlderJava.absurdityAppForJava.domain.order.service.response.FindPayedOrdersResponse;
+import forOlderJava.absurdityAppForJava.domain.payment.service.request.FindPayedOrdersCommand;
 import forOlderJava.absurdityAppForJava.domain.younger.service.ErrandService;
 import forOlderJava.absurdityAppForJava.domain.younger.service.request.RegisterErrandCommand;
 import org.junit.jupiter.api.*;
@@ -18,6 +22,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -67,5 +72,30 @@ public class OrderFlowIntegrationTest {
         FindOrdersResponse findOrderResponse = orderService.findOrders(testMemberId, 0);
         assertThat(findOrderResponse.orders()).hasSize(1);
         assertThat(findOrderResponse.totalPages()).isEqualTo(1);
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("2. 심부름꾼의 결제완료 주문 조회 테스트")
+    void findPayedOrderTest() {
+        FindPayedOrdersCommand command = FindPayedOrdersCommand.builder()
+                .memberId(testMemberId)
+                .page(0)
+                .build();
+
+        FindPayedOrdersResponse response = orderService.findPayedOrders(command);
+
+        assertThat(response.page()).isZero();
+        assertThat(response.orders())
+                .extracting("status")
+                .containsOnly(OrderStatus.PAYING);
+
+        FindPayedOrdersCommand invalidCommand = FindPayedOrdersCommand.builder()
+                .memberId(testMemberId)
+                .page(0)
+                .build();
+
+        assertThrows(UnauthorizedOrderException.class, () ->
+                orderService.findPayedOrders(invalidCommand));
     }
 }
